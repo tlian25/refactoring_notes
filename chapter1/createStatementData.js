136c1d69067f7ff1597fbc3d0c2c854ff29a2329
tlian25 <tlian25@github>
2022-05-18 08:51:10 -0400

    update

---
+++ chapter1/createStatementData.js
@@ -0,0 +1,70 @@
+// create statement data
+export function createStatementData(invoice, plays) {
+
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+
+    // make a copy of performance object
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
+        return result;
+    }
+
+
+    // get play details based on performance
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    // replace switch statement with amountFor function
+    function amountFor(aPerformance) {
+        let result = 0;
+
+        switch (aPerformance.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${aPerformance.play.type}`)
+        }
+        return result;
+    }
+
+
+    // calculate volume credits
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        //add volume credits
+        result += Math.max(aPerformance.audience - 30, 0);
+        //add extra credit for every 10 comedy attendees
+        if (aPerformance.play.type === "comedy") result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    // embed loop over volume credits
+    function totalVolumeCredits(data) {
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+    }
+
+    // embed loop over total amount
+    function totalAmount(data) {
+        return data.performances.reduce((total, p) => total + p.amount, 0);
+    }
+}
---
+++ chapter1/statement.js
@@ -0,0 +1,43 @@
+// Refactored Example
+
+import {createStatementData} from './createStatementData.js';
+
+function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+
+
+function renderPlainText(data, plays) {
+
+    // Main function portion
+    let result = `Statement for ${data.customer}\n`;    
+
+    for (let perf of data.performances) {
+        //print line for this order
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${data.totalVolumeCredits} credits \n`;
+    return result;
+
+
+    // HELPER FUNCTIONS - REFACTORINGS
+
+    // format number
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {style: "currency", currency: "USD",
+            minimumFractionDigits: 2}).format(aNumber / 100);
+    }
+}
+
+
+
+invoice = require("./invoices.json")[0]
+plays = require("./plays.json")
+
+var result = statement(invoice, plays);
+console.log();
+console.log(result);
